9e484d20b0434b9f7b4d7aec3891c4a1505618aa
huangjiaqi <huangjiaqi@digi-plus.cn>
2024-05-27 18:55:01 +0800

    feat: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import Layout from '@/components/layout'
+import Image from 'next/image'
 import React from 'react'
 
 const Index = () => {
   return (
     <Layout title='QQ-轻松做自己'>
       <div style={{
-        // height:'100vh'
-      }} className='w-screen h-screen bg-blue-200 flex justify-center items-center'>
-        
+        height:'100vh',
+      }} className='w-screen h-screen flex justify-center items-center'>
+        <img style={{
+          filter:'blur(150px)'
+        }} className='absolute z-0 top-0 left-0 w-full h-full' src="https://static-res.qq.com/web/im.qq.com/qq9_introduction_poster.jpg" alt="" />
+        <div style={{
+          backgroundColor:'#00000030'
+        }} className='absolute z-0 top-0 left-0 w-full h-full'></div>
+        {/* 内容主体 */}
+        <div className='absolute top-0 left-0 w-full min-h-full z-[1]'>
+            <h1>全新版本</h1>
+            <h1 className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-bold text-white'>QQ9</h1>
+        </div>
       </div>
     </Layout>
   )
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,47 @@
-import Link from 'next/link'
-import React from 'react'
-import logo from '@/assets/logo.png'
-import Image from 'next/image'
+import Link from "next/link";
+import React from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
 
-const currentBg = 'hsla(0, 0%, 100%, .5)'
+const currentBg = "hsla(0, 0%, 100%, .5)";
 
 const Header = () => {
   return (
-    <header className='absolute top-0 left-0 w-full h-[64px] border-b-[2px] border-solid border-[#fff50] flex items-center  px-[56px]'>
-        <div className='topside h-full flex items-center justify-between'>
-            <Link href={'/'}>
-                {/* qq logo */}
-                <Image priority={true} className='h-[24px] w-[55px]' src={logo} alt="qq logo" />
-            </Link>
-            <ul className='ml-[47px] flex text-white '>
-                <li className='px-[13px] h-[30px] flex justify-center items-center rounded-full hover:text-[#09f]'>首页</li>
-                <li className='px-[13px] h-[30px] flex justify-center items-center rounded-full hover:text-[#09f]'>下载</li>
-            </ul>
-        </div>
-        <div className='topTool'>
-
-        </div>
+    <header className="absolute z-[1] top-0 left-0 w-full h-[64px] border-b-[1px] border-solid justify-between border-[#fff50] flex items-center  px-[56px]">
+      <div className="topside h-full flex items-center ">
+        <Link href={"/"}>
+          {/* qq logo */}
+          <Image
+            fetchPriority={"high"}
+            priority={true}
+            className="h-[22px] w-[52px]"
+            src={logo}
+            alt="qq logo"
+          />
+        </Link>
+        <ul className="ml-[47px] flex text-white text-[14px]">
+          <li
+            style={{
+              background: `${currentBg}`,
+            }}
+            className="px-[13px] h-[30px] flex justify-center cursor-pointer items-center rounded-full hover:text-[#09f]"
+          >
+            首页
+          </li>
+          <li className="ml-[47px] px-[13px] h-[30px] flex justify-center cursor-pointer items-center rounded-full hover:text-[#09f]">
+            下载
+          </li>
+        </ul>
+      </div>
+      <div className="topTool flex items-center ">
+        <ul className="flex text-white text-[14px]">
+          <li className="hover:text-[#09f] cursor-pointer">QQ会员</li>
+          <li className="hover:text-[#09f] cursor-pointer ml-[60px]">QQ安全</li>
+          <li className="hover:text-[#09f] cursor-pointer ml-[60px]">登录</li>
+        </ul>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
